cdb0e4f8ddc0be25fb56d46c693fd737c56fa7ca
Vicky Daiya <37933641+vickydaiya@users.noreply.github.com>
2022-12-16 15:24:23 -0500

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -11,6 +11,16 @@ cd hubmap-kidney
 Rscript run_pipeline.R;
 cd ..;
 cd ..;
-sh tissue-bar-graphs/RUN.sh;
+
+mkdir tissue-bar-graphs/azimuth-predictions;
+cp -R hra-azimuth-predictions/ tissue-bar-graphs/azimuth-predictions/;
+cd tissue-bar-graphs;
+npm install utils/package.json;
+cd utils;
+node azimuth_aggregator.js;
+cd ..;
+python3 azimuth-predictions/hubmap-kidney/append_donor_metadata.py;
+cd ..;
+
 
 
